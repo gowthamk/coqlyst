ca31335719b31c5ae9a5430c2f0fd1490828c5b4
gowthamk <gowtham@purdue.edu>
2014-05-01 22:31:33 -0400

    Robs concat proof

--- exp.v
+++ exp.v
@@ -115,8 +115,11 @@ Module mylist2.
   
   Inductive CrossPrd (A B : Type) (s1 : Ensemble A) (s2 : Ensemble B) 
       : Ensemble (A * B) :=
-    CrossPrdIntro : forall x:A, forall y:B, In A s1 x -> In B s2 y -> 
+    CrossPrd_intro : forall x:A, forall y:B, In A s1 x -> In B s2 y -> 
                                             In (A*B) (CrossPrd s1 s2) (x,y).
+  
+  Hint Constructors CrossPrd.
+
   Fixpoint elts (A : Type)(l : list A) : Ensemble _:=
     match l with [] => Empty_set _ | x::xs => Union _ (Singleton _ x) (elts xs) end.
 
@@ -157,6 +160,7 @@ Module mylist2.
   Hint Resolve empty_set_is_ident.
   Hint Resolve union_is_associative.
 
+
   Theorem union_is_commutative : forall A : Type, forall s1 s2 : Ensemble A,
                                    Union _ s1 s2 = Union _ s2 s1.
   Proof.
@@ -187,7 +191,7 @@ Module mylist2.
 
   Require Import CpdtTactics.
 
-    Theorem rev_preserves_elts2 : forall A : Type, forall l : list A, 
+  Theorem rev_preserves_elts2 : forall A : Type, forall l : list A, 
                                  elts l = elts (rev l ).
   Proof.
     intros; list_cases(induction l as [| x xs]) Case; trivial.
@@ -197,6 +201,108 @@ Module mylist2.
       crush.
   Qed.
 
+  Notation "{( x )}" := (Singleton _ x) (at level 60).
+  Notation "x ** y" := (CrossPrd x y) (at level 65).
+  Fixpoint Robs (A : Type) (l : list A) : Ensemble (A*A) := 
+    match l with [] => Empty_set _ 
+     | x::xs => Union _ (CrossPrd ({(x)}) (elts xs)) (Robs xs) end.
+
+  Eval simpl in Robs ([1,2,3]).
+
+  Fixpoint Roas (A : Type) (l : list A) : Ensemble (A*A) := 
+    match l with [] => Empty_set _ 
+     | x::xs => Union _ (CrossPrd (elts xs) ({(x)})) (Roas xs) end.
+
+  Eval simpl in Roas ([1,2,3]).
+  
+  Check CrossPrd.
+  Check Robs.
+  Theorem empty_set_cross_prd : forall A B : Type, forall s : Ensemble A,
+                                  CrossPrd s (Empty_set B) = Empty_set (A*B) /\
+                                  CrossPrd (Empty_set B) s = Empty_set (B*A).
+  Proof.
+    intros. split; apply Extensionality_Ensembles; unfold Same_set; 
+    unfold Included; split; intros ? H; inversion H; try (inversion H1); 
+    try (inversion H0).
+  Qed.
+
+  Hint Resolve empty_set_cross_prd.
+  
+  Theorem cross_prd_distributes : forall A B : Type, forall s1 : Ensemble A,
+    forall s2 s3 : Ensemble B, 
+      s1 ** (Union _ s2 s3) = Union _ (s1 ** s2) (s1 ** s3) /\
+      (Union _ s2 s3) ** s1 = Union _ (s2 ** s1) (s3 ** s1).
+  Proof.
+    intros. split; apply Extensionality_Ensembles; unfold Same_set; 
+      split; unfold Included; intros ? H; inversion H; subst;
+      try(inversion H1); try (inversion H0); auto with *.
+  Qed.
+
+  Theorem cross_prd_distributesr : forall A B : Type, forall s1 : Ensemble A,
+    forall s2 s3 : Ensemble B, 
+      s1 ** (Union _ s2 s3) = Union _ (s1 ** s2) (s1 ** s3).
+  Proof. intros; apply cross_prd_distributes. Qed.
+
+  Theorem cross_prd_distributesl : forall A B : Type, forall s1 : Ensemble A,
+    forall s2 s3 : Ensemble B, 
+      (Union _ s2 s3) ** s1 = Union _ (s2 ** s1) (s3 ** s1).
+  Proof. intros; apply cross_prd_distributes. Qed.
+ 
+  Hint Resolve cross_prd_distributesl.
+  Hint Resolve cross_prd_distributesr.
+  Hint Rewrite cross_prd_distributesl.
+  Hint Rewrite cross_prd_distributesr.
+  Hint Rewrite union_is_associative.
+  Hint Rewrite union_is_commutative.
+
+ 
+  Notation "x @ y" := (Union _ x y) (at level 69, left associativity).
+
+  Require Import LibTactics.
+  Theorem concat_robs : forall A : Type, forall l1 l2 : list A, 
+    Robs (l1 ++ l2) = ((Robs(l1)) @ (Robs(l2))) @
+                           ((elts l1) ** (elts l2)).
+  Proof.
+    intros; list_cases(induction l1 as [|x1 xs1]) Case; simpl.
+    Case "Nil".
+      assert (CrossPrd (Empty_set A) (elts l2) = Empty_set (A*A)).
+        apply empty_set_cross_prd.
+      rewrite H. rewrite union_is_commutative. 
+      repeat (rewrite <- empty_set_is_ident); trivial.
+    Case "Cons".
+      rewrite concat_preserves_elts. 
+      rewrite IHxs1.
+      assert ( ({(x1)} @ (elts xs1)) ** elts l2 = 
+              ({(x1)} ** (elts l2)) @ ((elts xs1) ** (elts l2))).
+        auto.
+      rewrite H.
+      assert ({(x1)} ** (elts xs1 @ elts l2) = 
+              {(x1)} ** (elts xs1) @ {(x1)} ** (elts l2)).
+        auto.
+      rewrite H0. repeat (rewrite <- union_is_associative).
+      assert ({(x1)} ** elts xs1 @ {(x1)} ** elts l2 @ Robs xs1 = 
+              {(x1)} ** elts xs1 @ ({(x1)} ** elts l2 @ Robs xs1)).
+        auto.
+      rewrite H1.
+      assert ({(x1)} ** elts l2 @ Robs xs1 = Robs xs1 @ {(x1)} ** elts l2).
+        auto.
+      rewrite H2.
+      assert ({(x1)} ** elts xs1 @ (Robs xs1 @ {(x1)} ** elts l2) = 
+              {(x1)} ** elts xs1 @ Robs xs1 @ {(x1)} ** elts l2).
+        auto.
+      rewrite H3.
+      assert ({(x1)} ** elts xs1 @ Robs xs1 @ {(x1)} ** elts l2 @ Robs l2 = 
+              {(x1)} ** elts xs1 @ Robs xs1 @ ({(x1)} ** elts l2 @ Robs l2)).
+        auto.
+      rewrite H4.
+      assert (({(x1)} ** elts l2 @ Robs l2) = (Robs l2 @ {(x1)} ** elts l2)).
+        auto.
+      rewrite H5.
+      assert ({(x1)} ** elts xs1 @ Robs xs1 @ (Robs l2 @ {(x1)} ** elts l2) =
+              {(x1)} ** elts xs1 @ Robs xs1 @ Robs l2 @ {(x1)} ** elts l2).
+        auto.
+      rewrite H6. reflexivity.
+  Qed.
 
 End mylist2.
 
